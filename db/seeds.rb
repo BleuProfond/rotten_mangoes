1ccd411beb38614404849ce04b31030dd0e5ebed
BleuProfond <p.verteuil@gmail.com>
2016-03-01 03:55:43 +0000

    added MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create!(
+  title: "Wayne's World",
+  director: "Penelope Spheeris",
+  runtime_in_minutes: 94,
+  description: "Two slacker friends try to promote their public-access cable show.",
+  poster_image_url: "http://clashmusic.com/sites/default/files/styles/article_feature/public/field/image/waynes-world-3.jpg?itok=PIbjt3FO",
+  release_date: "14/02/1992"
+)
