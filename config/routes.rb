d6f3278ef068de7ca4107cc81257fb1b7c26922d
BleuProfond <p.verteuil@gmail.com>
2016-03-01 19:24:04 +0000

    user can submit movie reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 RottenMangoes::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "users/new"
-  get "users/create"
-  resources :movies
-  root to: 'movies#index'
 
+  get "reviews/new"
+  get "reviews/create"
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  
 end
