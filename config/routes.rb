01b4a998b9cb9291b38734ea8c5030a44c5762bd
BleuProfond <p.verteuil@gmail.com>
2016-03-01 18:06:28 +0000

    users can create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 RottenMangoes::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
   get "users/new"
   get "users/create"
   resources :movies
   root to: 'movies#index'
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
